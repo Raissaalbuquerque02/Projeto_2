3a1ccf310129d10d3e4afa7ff333f333a2fb0230
Raissaalbuquerque02 <raissa.albuquerquee02@gmail.com>
2025-06-30 19:08:31 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirFormulario() {
+  window.open("formulario.html", "_blank");
+}
+
+function abrirFrutas() {
+  window.open("manipulando_fruta.html", "_blank");
+}
+
+function abrirJogo() {
+  window.open("jogo.html", "_blank");
+}
